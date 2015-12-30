270520e2a58de028078f7ba6789f9ca9994349af
rick smit <rick@vigour.io>
2015-12-30 21:31:12 +0100

    refactored url so i uncerstand

--- test/common/index.js
+++ test/common/index.js
@@ -1,68 +1,66 @@
-var theUrl = require('../../lib/index.js')
-var colors = require('colors-browserify') //eslint-disable-line
+var Element = require('vigour-element')
 var Observable = require('vigour-js/lib/observable')
 
-//
-theUrl.inject(
-  require('../../lib/browser.js')
-)
+var router = require('../../lib')
 
-console.log(theUrl)
-
-global.url = new theUrl.Constructor({
-
-})
-
-var a = global.bla = new Observable({
-  val: 'yo'
-})
-var b = global.bla2 = new Observable('yo2')
-
-global.url.set({
-  // inject: [
-  //   require('vigour-router/lib/browser.js')
-  // ], y u no work
-  search: 'searchit!',
-  'hello': {
-    separator: ',',
-    indicator: '#',
-    on: {
-      data (data) {
-        if (!this.val) {
-          console.log('ok update my bitches! shit be gone!'.red)
-        } else {
-          console.log('data creepin!'.white.bold.inverse, this.val)
-        }
-      }
-    },
-    val: a
-  },
-  '💩': {
-    separator: '&',
-    indicator: '#',
-    on: {
-      data (data) {
-        if (!this.val) {
-          console.log('ok update my bitches! shit be gone!'.red)
-        } else {
-          console.log('data creepin! burs'.magenta.inverse, data, this.val)
-        }
-      }
-    },
-    val: b
-  },
-  'no': {
-    separator: ',',
-    indicator: '/',
-    on: {
-      data (data) {
-        if (!this.val) {
-          console.log('ok update my bitches! shit be gone!'.red)
-        } else {
-          console.log('data creepin! burs'.magenta.inverse, data, this.val)
-        }
-      }
-    },
-    val: b
+var pages = new Element({
+  val: router.navigation.page,
+  on: {
+    data () {
+      // console.log('go to the correct pages', router.url.val)
+    }
   }
 })
+
+var shows = new Element({
+  val: router.navigation.show,
+  on: {
+    data () {
+      // console.log('go to the correct show page', router.url.val)
+    }
+  }
+})
+
+var modal = new Element({
+  val: router.modal,
+  on: {
+    data () {
+      // console.log('do something!!', router.modal.val)
+    }
+  }
+})
+
+it('should update window hash when setting modal', function (done) {
+  router.modal.val = {
+    open: 'register'
+  }
+  expect(window.location.search).to.contain('open=register')
+  done()
+})
+
+it('should remove querystring when removed', function (done) {
+  router.modal.close('register')
+  expect(window.location.search).to.not.contain('open=register')
+  done()
+})
+
+it('should update path when setting page', function (done) {
+  router.navigation.val = 'discover'
+  expect(window.location.pathname).to.equal('/discover')
+  done()
+})
+
+var show = new Observable({
+  title: 'andere show',
+  season: 1,
+  episode: 1
+})
+
+it('should update path when setting show property', function (done) {
+  router.navigation.val = show
+  // expect(window.location.pathname).to.equal('/no')
+  done()
+})
+
+exports.pages = pages
+exports.modal = modal
